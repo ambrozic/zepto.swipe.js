a35f17c34632d301783aae319652adabe47809cc
ambrozic <ambrozic@gmail.com>
2012-04-16 12:57:42 +0200

    deploy on github

---
+++ README.md
@@ -0,0 +1,50 @@
+###description:###
+swipe.js - zepto plugin for minimal implementation of swipe functionality
+
+- pretty much still a beta, work in progress.
+- a lot of missing functionality like allow page scroll, multi touch etc.
+
+
+###usage###
+
+$(selector).swipe({
+    status:function (event, phase, direction, distance) {
+        //code
+    })
+});
+
+Global setup:
+
+ Set up git
+  git config --global user.name "Your Name"
+  git config --global user.email ambrozic@gmail.com
+
+
+Next steps:
+
+  mkdir swipe.js
+  cd swipe.js
+  git init
+  touch README
+  git add README
+  git commit -m 'first commit'
+  git remote add origin git@github.com:ambrozic/swipe.js.git
+  git push -u origin master
+
+
+Existing Git Repo?
+
+  cd existing_git_repo
+  git remote add origin git@github.com:ambrozic/swipe.js.git
+  git push -u origin master
+
+
+Importing a Subversion Repo?
+
+  Check out the guide for step by step instructions.
+
+
+When you're done:
+
+  Continue
+
---
+++ src/swipe.js
@@ -0,0 +1,86 @@
+/*
+ * swipe.js -  zepto plugin for minimal implementation of swipe functionality
+ *
+ * (c) Gregor Ambrozic (sensehack.com)
+ * 
+ * @version 0.1
+ */
+
+(function ($) {
+    $.fn.swipe = function (options) {
+        if (!this)return false;
+        var touchable = "ontouchstart" in window,
+            START = "start",
+            MOVE = "move",
+            END = "end",
+            LEFT = "left",
+            RIGHT = "right",
+            UP = "up",
+            DOWN = "down",
+            phase = START
+            ;
+        return this.each(function () {
+            var self = this,
+                $self = $(this),
+                start = {x:0, y:0},
+                end = {x:0, y:0},
+                delta = {x:0, y:0},
+                distance = {x:0, y:0},
+                direction = undefined,
+                touches = 0
+                ;
+
+            function validate(event) {
+                var evt = touchable ? event.touches[0] : event;
+                distance.x = evt.pageX - start.x;
+                distance.y = evt.pageY - start.y;
+                delta.x = evt.pageX - end.x;
+                delta.y = evt.pageY - end.y;
+                end.x = evt.pageX;
+                end.y = evt.pageY;
+
+                var angle = Math.round(Math.atan2(end.y - start.y, start.x - end.x) * 180 / Math.PI);
+                if (angle < 0) angle = 360 - Math.abs(angle);
+                if ((angle <= 45) && (angle >= 0) || (angle <= 360) && (angle >= 315)) {
+                    direction = LEFT;
+                } else if ((angle >= 135) && (angle <= 225)) {
+                    direction = RIGHT;
+                } else if ((angle > 45) && (angle < 135)) {
+                    direction = DOWN;
+                } else {
+                    direction = UP;
+                }
+            }
+
+            function swipeStart(event) {
+                var evt = touchable ? event.touches[0] : event;
+                if (touchable) touches = event.touches.length;
+                phase = START;
+                start.x = evt.pageX;
+                start.y = evt.pageY;
+                validate(event);
+
+                self.addEventListener((touchable) ? "touchmove" : "mousemove", swipeMove, false);
+                self.addEventListener((touchable) ? "touchend" : "mouseup", swipeEnd, false);
+
+                if (options.status) options.status.call($self, event, phase, direction, distance);
+            }
+
+            function swipeMove(event) {
+                //todo implement page scrolling
+                event.preventDefault();
+                if (phase === END) return;
+                phase = MOVE;
+                validate(event);
+                if (options.status) options.status.call($self, event, phase, direction, distance);
+            }
+
+            function swipeEnd(event) {
+                phase = END;
+                if (options.status) options.status.call($self, event, phase, direction, distance);
+            }
+
+            self.addEventListener((touchable) ? "touchstart" : "mousedown", swipeStart, false);
+        });
+    }
+})(window.Zepto);
